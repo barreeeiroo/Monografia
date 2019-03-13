a72f2794439ab2a55f517b28dacf7acedf6b6e1f
Diego Barreiro <diego.barreiro.perez@gmail.com>
2019-03-13 02:57:34 +0100

    UX: Init Script

--- fourFactors/script.py
+++ fourFactors/script.py
@@ -3,15 +3,15 @@
 
 
 ##############################################################################################
-#                                         RECOMMENDED                                        #
-#                It is highly advised to read the following iPython notebook,                #
-#                     as it contains the same code but better commented.                     #
+#                                         RECOMENDADO                                        #
+#                  Se recomienda leer la siguiente iPython notebook, ya que                  #
+#                     contiene el mismo código pero mucho más comentado.                     #
 #   https://github.com/barreeeiroo/Monografia/blob/master/fourFactors/Four%20Factors.ipynb   #
 ##############################################################################################
 
 
 
-# We make all required imports
+# Se hacen todos los imports
 import os
 from random import randint
 import pandas as pd
@@ -19,32 +19,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-# We load the CSV file containing all stats
+# Se carga el archivo CSV
 wdir = os.path.dirname(__file__)
 data = pd.read_csv(wdir+'/fourFactors.csv')
 
-# Declare the desired team axes
+# Se declaran los ejes de la función
 x = data[['efg', 'ftr', 'orb', 'tov']]
 y = data['scr']
-# And the opponent one
+# Y los del equipo contrario
 x_opp = data[['efg_opp', 'ftr_opp', 'orb_opp', 'tov_opp']]
 y_opp = data['scr_opp']
 
-# We designate a random integer in order to split the data
+# Se designa un número aleatorio para generar la muestra
 random = randint(0, 99)
-# We split the data, so we keep some of them for testing purposes
+# Se separan los datos, para no sobrealimentar el modelo
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=random)
 x_train_opp, x_test_opp, y_train_opp, y_test_opp = train_test_split(x_opp, y_opp, random_state=random)
 
-# Initialize the LinearRegression method
+# Se inicia el método de regresión linear
 estimate = LinearRegression()
 estimate_opp = LinearRegression()
 
-# MODEL TRAINING
+# ENTRENAMIENTO DEL MODELO
 estimate.fit(x_train, y_train)
 estimate_opp.fit(x_train_opp, y_train_opp)
 
-# Print the weight of each factor
+# Se muestran los coeficientes y el valor independiente
 print(estimate.coef_)
 print(estimate_opp.coef_)
 # print(estimate.intercept_)
@@ -59,17 +59,17 @@ y_pred_opp = estimate_opp.predict(x_test_opp)
 # print(y_test_opp)
 # print(y_pred_opp)
 
-# Some error output for debugging purposes
+# Muestra de errores para debug del programa
 # print(mean_squared_error(y_test, y_pred))
 # print(r2_score(y_test, y_pred))
 
-# We have to migrate the int64 format from numpy to a list so we can iterate it
+# Hace falta migrar el formato int64 de numpy a una lista para poder iterarla
 p_results = y_pred.tolist()
 p_results_opp = y_pred_opp.tolist()
 results = y_test.tolist()
 results_opp = y_test_opp.tolist()
 
-# Initialize the array containing the victories
+# Se inician las listas conteniendo las victorias
 p_won = []
 won = []
 
@@ -79,6 +79,6 @@ while counter < len(p_results):
     won.append(1 if results[counter]>results_opp[counter] else 0)
     counter += 1
 
-# We print the results of the predictions
+# Se muestran los resultados de las predicciones
 print(p_won)
 print(won)
